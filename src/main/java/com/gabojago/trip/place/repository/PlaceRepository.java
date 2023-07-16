1719bf9cbfefc947854c83b360937db1a6210452
yoonwoo-kim <ynwookim98@gmail.com>
2023-07-16 21:50:59 +0900

    Fix: Pageable 변수 인자에 추가(Repository)

--- src/main/java/com/gabojago/trip/place/repository/PlaceRepository.java
+++ src/main/java/com/gabojago/trip/place/repository/PlaceRepository.java
@@ -2,6 +2,7 @@ package com.gabojago.trip.place.repository;
 
 import com.gabojago.trip.place.domain.Place;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,5 @@ public interface PlaceRepository extends JpaRepository<Place, Integer> {
 
     // 가장 많이 스크랩된 3개 장소의 id 리스트 정보
     @Query("SELECT ps.placeId FROM PlaceScrap ps GROUP BY ps.placeId ORDER BY count(ps.placeId) DESC")
-    List<Integer> findTop3ScrappedPlacesId();
+    List<Integer> findTop3ScrappedPlacesId(Pageable pageable);
 }
