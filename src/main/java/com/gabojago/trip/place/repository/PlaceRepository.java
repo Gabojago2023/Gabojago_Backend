cb2ef8838011828826735cacd8b1b4e0daa4cbab
yoonwoo-kim <ynwookim98@gmail.com>
2023-07-16 21:26:00 +0900

    Feat: 가장 많이 스크랩된 Top3 장소 정보 조회(Repository) - id로 장소DTO 정보 얻기 - 가장 많이 스크랩된 3개 장소의 id 리스트 정보 얻기

--- src/main/java/com/gabojago/trip/place/repository/PlaceRepository.java
+++ src/main/java/com/gabojago/trip/place/repository/PlaceRepository.java
@@ -1,10 +1,18 @@
 package com.gabojago.trip.place.repository;
 
 import com.gabojago.trip.place.domain.Place;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlaceRepository extends JpaRepository<Place,Long> {
+public interface PlaceRepository extends JpaRepository<Place, Integer> {
 
+    // id로 장소 정보 얻기
+    Place findById(int id);
+
+    // 가장 많이 스크랩된 3개 장소의 id 리스트 정보
+    @Query("SELECT ps.placeId FROM PlaceScrap ps GROUP BY ps.placeId ORDER BY count(ps.placeId) DESC")
+    List<Integer> findTop3ScrappedPlacesId();
 }
