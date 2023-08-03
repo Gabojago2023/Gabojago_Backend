3da2a548b519013fb267500156ddad62e2b6ac20
yoonwoo-kim <ynwookim98@gmail.com>
2023-08-04 00:52:35 +0900

    feat: Comment, PlaceHistory 엔티티 추가

---
+++ src/main/java/com/gabojago/trip/place/domain/Comment.java
@@ -0,0 +1,37 @@
+package com.gabojago.trip.place.domain;
+
+import com.gabojago.trip.user.domain.User;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@NoArgsConstructor
+@ToString
+@Getter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String commentText;
+    private Integer starRating;
+
+    @CreationTimestamp
+    private java.sql.Timestamp createdDate;
+}
