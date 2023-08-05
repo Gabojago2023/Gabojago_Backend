03f389a0efd2f8d3ea69c53f08c4e023f599f3db
yoonwoo-kim <ynwookim98@gmail.com>
2023-08-05 17:14:36 +0900

    fix: 주어진 키워드로 검색하게끔 수정

--- src/main/java/com/gabojago/trip/place/service/PlaceServiceImpl.java
+++ src/main/java/com/gabojago/trip/place/service/PlaceServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PlaceServiceImpl implements PlaceService{
-    private PlaceRepository placeRepository;
+    private final PlaceRepository placeRepository;
 
     @Autowired
     public PlaceServiceImpl(PlaceRepository placeRepository) {
@@ -20,11 +20,10 @@ public class PlaceServiceImpl implements PlaceService{
     public List<PlaceResponseDto> searchAttractionByKeyword(Integer userId, Integer sidoCode,
             Integer gugunCode, String keyword) {
         List<PlaceResponseDto> placeResponseDtoList = new ArrayList<>();
-        List<Object[]> placesByFilter = placeRepository.findPlacesByFilter(userId, sidoCode, gugunCode, "공원");
+        List<Object[]> placesByFilter = placeRepository.findPlacesByFilter(userId, sidoCode, gugunCode, keyword);
         for(Object[] o : placesByFilter) {
             PlaceResponseDto from = PlaceResponseDto.from(o);
             placeResponseDtoList.add(from);
-            System.out.println(from);
         }
         return placeResponseDtoList;
     }
