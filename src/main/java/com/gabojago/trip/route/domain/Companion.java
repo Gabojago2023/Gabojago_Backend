11ddc348991cacfe7c6328ebb02810323051e52a
Eundms <dms011221@gmail.com>
2023-08-06 17:08:34 +0900

    add: entity related route

--- src/main/java/com/gabojago/trip/route/domain/Companion.java
+++ src/main/java/com/gabojago/trip/route/domain/Companion.java
@@ -2,17 +2,22 @@ package com.gabojago.trip.route.domain;
 
 import com.gabojago.trip.user.domain.User;
 import javax.persistence.Entity;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import static javax.persistence.EnumType.STRING;
 
 @Entity
 public class Companion {
 
     @Id
     private Integer id;
+    @Enumerated(STRING)
+    private CompanionGrant grant; // 권한 레벨
     @ManyToOne
-    private TripRoute tripRoute;
+    private TripRoute tripRoute; // 여행계획아이디
     @ManyToOne
-    private User user; // 동행자
-    private String grant;
+    private User user; // 동행자아이디
+
 }
---
+++ src/main/java/com/gabojago/trip/route/domain/CompanionGrant.java
@@ -0,0 +1,5 @@
+package com.gabojago.trip.route.domain;
+public enum CompanionGrant {
+    WRITER, // Post, Get
+    READER // Get
+}
