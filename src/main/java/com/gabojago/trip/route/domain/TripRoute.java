c5bc2b117fb71d219233dc3357723e8c62d7d018
Eundms <dms011221@gmail.com>
2023-08-07 00:38:00 +0900

    fix: Companion.grant -> Companion.companionGrant 예약어 제거

--- src/main/java/com/gabojago/trip/route/domain/TripRoute.java
+++ src/main/java/com/gabojago/trip/route/domain/TripRoute.java
@@ -2,12 +2,7 @@ package com.gabojago.trip.route.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import com.gabojago.trip.route.dto.TripRouteCreateDto;
 import com.gabojago.trip.route.dto.TripRouteModifyDto;
@@ -22,7 +17,6 @@ public class TripRoute {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Integer id;
 
     private String name;
@@ -33,7 +27,8 @@ public class TripRoute {
     private boolean isPublic;
     private LocalDateTime createdAt;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Builder
