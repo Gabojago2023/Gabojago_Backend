7f16a368361cbdc48d3c1308e9f502bbd0207702
Yang <haeundae96@gmail.com>
2023-08-06 20:40:53 +0900

    feat: one pick service implement

--- src/main/java/com/gabojago/trip/onepick/service/OnePickService.java
+++ src/main/java/com/gabojago/trip/onepick/service/OnePickService.java
@@ -1,8 +1,23 @@
 package com.gabojago.trip.onepick.service;
 
+import com.gabojago.trip.onepick.domain.DistributedOnePick;
 import com.gabojago.trip.onepick.domain.OnePick;
 
 public interface OnePickService {
 
-    // OnePick 조회
+    // OnePick 하나 뽑기
+    OnePick getOnePick(String category, String baseLocation, Integer userId);
+
+    // 나의 모든 DistributedOnePick 조회
+    DistributedOnePick[] getAllDistributedOnePick(Integer userId);
+
+    // 내가 등록한 원픽 장소 조회
+    OnePick[] getAllOnePick(Integer userId);
+
+    // 원픽 장소 등록
+    void addOnePick(String description, Integer place, Integer user, Integer category, Integer baseLocation);
+
+    // 원픽 장소 수정
+    void modifyOnePick(Integer onePickId, String description, Integer place, Integer user, Integer category, Integer baseLocation);
+
 }
--- src/main/java/com/gabojago/trip/onepick/service/OnePickServiceImpl.java
+++ src/main/java/com/gabojago/trip/onepick/service/OnePickServiceImpl.java
@@ -1,5 +1,58 @@
 package com.gabojago.trip.onepick.service;
 
-public class OnePickServiceImpl {
+import com.gabojago.trip.onepick.domain.DistributedOnePick;
+import com.gabojago.trip.onepick.domain.OnePick;
+import com.gabojago.trip.onepick.repository.OnePickRepository;
 
+public class OnePickServiceImpl implements OnePickService {
+
+    private final OnePickRepository onePickRepository;
+
+    public OnePickServiceImpl(OnePickRepository onePickRepository) {
+        this.onePickRepository = onePickRepository;
+    }
+
+    // OnePick 하나 뽑기
+    @Override
+    public OnePick getOnePick(String category, String baseLocation, Integer userId) {
+        OnePick onePick;
+        do {
+            if (category != null && baseLocation != null) {
+                onePick = onePickRepository.findOnePickByCategoryAndBaseLocation(category, baseLocation);
+            } else if (category != null) {
+                onePick = onePickRepository.findOnePickByCategory(category);
+            } else if (baseLocation != null) {
+                onePick = onePickRepository.findOnePickByBaseLocation(baseLocation);
+            } else {
+                return null;
+            }
+        } while (onePickRepository.existsDistributedOnePickByOnePickIdAndUserId(onePick.getId(), userId));
+
+        return onePick;
+    }
+
+    // 나의 모든 DistributedOnePick 조회
+    @Override
+    public DistributedOnePick[] getAllDistributedOnePick(Integer userId) {
+        return onePickRepository.findAllDistributedOnePickByUserId(userId);
+    }
+
+    // 내가 등록한 원픽 장소 조회
+    @Override
+    public OnePick[] getAllOnePick(Integer userId) {
+        return onePickRepository.findAllOnePickByUserId(userId);
+    }
+
+    // 원픽 장소 등록
+    @Override
+    public void addOnePick(String description, Integer place, Integer user, Integer category,
+            Integer baseLocation) {
+        onePickRepository.addOnePick(description, place, user, category, baseLocation);
+    }
+    // 원픽 장소 수정
+    @Override
+    public void modifyOnePick(Integer onePickId, String description, Integer place, Integer user,
+            Integer category, Integer baseLocation) {
+        onePickRepository.addOnePick(description, place, user, category, baseLocation);
+    }
 }
