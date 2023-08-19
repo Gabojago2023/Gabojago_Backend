b101dd35ab3bffb62aef7b3d3998d5c4d6c71db9
Yang <haeundae96@gmail.com>
2023-08-19 19:27:38 +0900

    fix: ticket need authorization and user id is decoded from the token

--- src/main/java/com/gabojago/trip/ticket/controller/TicketController.java
+++ src/main/java/com/gabojago/trip/ticket/controller/TicketController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,16 +26,23 @@ public class TicketController {
     }
 
     @PostMapping("/purchase")
-    public ResponseEntity<?> purchaseTicket(@RequestBody HashMap<String ,String> map) {
-        int id = Integer.parseInt(map.get("id"));
+//    public ResponseEntity<?> purchaseTicket(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<?> purchaseTicket() {
+        //        Integer userId = authService.getUserIdFromToken(token);
+        Integer id = 2;
+
         log.debug("[POST] /ticket/purchase " + id);
         // 여기서 추가적인 결제 인증 로직이 필요할 수 있음
         ticketService.addTicket(id);
         return new ResponseEntity<>("", HttpStatus.OK);
     }
 
-    @GetMapping("/count/{id}")
-    public ResponseEntity<?> getTicketCount(@PathVariable Integer id) {
+    @GetMapping("/count")
+//    public ResponseEntity<?> getTicketCount(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<?> getTicketCount() {
+        //        Integer userId = authService.getUserIdFromToken(token);
+        Integer id = 2;
+
         log.debug("[GET] /ticket/count/" + id);
         Integer count = ticketService.countTicketsByUserId(id);
         return new ResponseEntity<>(count, HttpStatus.OK);
