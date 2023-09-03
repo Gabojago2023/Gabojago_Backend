848ff3dec770a6f2ab2068bf0b9b1d6adc3d711a
Eundms <dms011221@gmail.com>
2023-09-03 21:42:30 +0900

    fix:

--- src/main/java/com/gabojago/trip/image/config/AmazonS3Config.java
+++ src/main/java/com/gabojago/trip/image/config/AmazonS3Config.java
@@ -12,14 +12,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AmazonS3Config {
 
-    //@Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey="";
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
 
-    //@Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey="";
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
 
-    //@Value("${cloud.aws.region.static}")
-    private String region="";
+    @Value("${cloud.aws.region.static}")
+    private String region;
 
     @Bean
     public AmazonS3Client amazonS3Client() {
